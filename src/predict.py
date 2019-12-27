3acf719165322af548dfc44d5e7d0ee5fb0e949a
Victor Wang <victorwang2001@gmail.com>
2019-12-26 22:45:42 -0500

    completed neural net

--- src/predict.py
+++ src/predict.py
@@ -1,19 +1,49 @@
 import tensorflow as tf
 from tensorflow import keras
-import pandas
+import pandas as pd
 import json
+from sklearn.model_selection import train_test_split
 
 JSONFile = open(r"data.txt","r")
 JSONText = JSONFile.read()
 
-#print(JSONText)
-
 jsonData = json.loads(JSONText)
 
-matchteam = jsonData['matchteam']
-print(matchteam)
+dataIn = pd.DataFrame(jsonData)
 
-for x in range(len(matchteam)):
-    print(matchteam[x])
+print("all dataIn")
+print(dataIn)
+
+#makes new tables filled with the expected outs
+dataOut = dataIn[["redscore", "bluescore", "redwin", "bluewin"]].copy()
+dataIn = dataIn.drop(["redscore", "bluescore", "redwin", "bluewin"], axis=1)
+
+dataInTrain, dataInTest, dataOutTrain, dataOutTest = train_test_split(dataIn, dataOut, test_size = 0.2)
+
+print("dataOut")
+print(dataOut)
+
+print("dataIn")
+print(dataIn)
+
+#dataIn.drop(["redscore", "bluescore"], axis=1)
+
+#modCol()
+
+model = keras.Sequential([
+    keras.layers.Input(shape=(len(dataInTrain.index), )),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(128, activation="relu"),
+    keras.layers.Dense(2, activation="softmax")])
+
+model.compile(optimizer="adam", loss="sparse_categorical_crossentropy", metrics=["accuracy"])
+
+model.fit(dataInTrain, dataOutTrain, epochs = 2)
+
+test_loss, test_acc = model.evaluate(datainTest, dataOutTest)
 
 
+
+def modCol(df, colId, func):
+    df[coldId] = df[coldId].apply(func)
+    
